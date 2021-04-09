031e691bbe44630b19f37c97a7bf99d39a23a4c9
Marcin_He <marhelle4@gmail.com>
2021-04-10 00:35:15 +0200

    Update_100421

---
+++ kaisercode.py
@@ -0,0 +1,18 @@
+
+#lan0$ = [ABCDEFGHIJKLMNOPRSTWUYZ]
+
+#lan1$ = [ABCDEFGHIJKLMNOPRSTUWYZ]
+
+#lan2$ = [AÄBCDEFGHIJKLMNOÖPRSTUÜWYZ]
+
+#lan3$ = [ABCDEFGHIJKLMNOPRSTUWYZ]
+
+print ("Witaj w systemie do wprowadzanie i kodowania wiadomosci Szyf Cezara")
+
+#Zbieranie danych
+
+language = input ("Wybierz uzywany przez ciebie jezyk 0 - Polski 1 - Angielski 2 - Niemiecki")
+
+step = input ("Podaj wybrany krok ")
+
+#Kodowanie
