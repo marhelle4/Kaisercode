d7ea79d3a7387fd36d99ca9e750daed81fa84848
Marcin_He <marhelle4@gmail.com>
2021-04-21 00:29:02 +0200

    Update_210421

--- kaisercode.py
+++ kaisercode.py
@@ -1,12 +1,29 @@
 
-lan0 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lan1 = lan0 + lan0
+lat0 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lat1 = lat0 + lat0
+
+deu0 = "AÄBCDEFGHIJKLMNOÖPQRSßTUÜVWXYZ"
+deu1 = deu0 + deu0
 
 print("Witaj w programie do kodowania wiadomosci wedlug szyfru Cezara")
-int krok = input ("Podaj twoj krok. max 26 ")
+klucz = input ("Podaj twoj klucz. max 26 ")
 
-#if krok > 26:
-#    print ("Twoj krok jest za duzy. Ale sproboj jeszcze raz")
-#    krok = input ("Podaj twoj krok. max 26 ")
+key = int(klucz)
 
-print(lan1[krok])
+if key > 26:
+    print ("Twoj krok jest za duzy. Ale sproboj jeszcze raz")
+    krok = input ("Podaj twoj klucz. max 26 ")
+
+print (lat1)
+message = input ("Wprowadz twoja wiadomosci do zakodowania: ")
+print(message)
+print (lat1)
+
+#Kodowanie wiadomosci
+size = len(message)
+size0 = int(size)
+
+print(size0)
+
+
+
