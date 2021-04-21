de8434e1383ab38ec3a466efcf2dee0894fd558a
Marcin_He <marhelle4@gmail.com>
2021-04-21 23:45:00 +0200

    Update_210421_2

--- kaisercode.py
+++ kaisercode.py
@@ -2,7 +2,7 @@
 lat0 = "abcdefghijklmnopqrstuvxyz"
 lat1 = lat0 + lat0
 
-deu0 = "AÄBCDEFGHIJKLMNOÖPQRSßTUÜVWXYZ"
+deu0 = "aäbcdefghijklmnoöpqrsßtuüvwxyz"
 deu1 = deu0 + deu0
 
 print("Witaj w programie do kodowania wiadomosci wedlug szyfru Cezara")
@@ -22,8 +22,11 @@ print (lat1)
 size = len(message)
 size0 = int(size)
 
-for i in range(1,size0):
+print("Dlugosc wiadomosci",size0)
+
+for i in range(0,size0):
     message0 = message[i]
+    print(message0)
     if message0 == ".":
         message0 = message0
     if message0 == ",":
@@ -33,7 +36,7 @@ for i in range(1,size0):
     if message0 == " ":
         message0 = message0
 
-    message1 = message0 + lat1[key]
+    message1 = message0 + lat1[i]
 
 print(message1)
 print(lat1)
